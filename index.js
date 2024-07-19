35b16cc9a25b2ce2be3b94b77c6f46b26f9c9c64
Victor-MGB <mgbemenaosonduv@gmail.com>
2024-07-19 19:06:24 +0100

    installed cors

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors()); // Add this line to enable CORS
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
